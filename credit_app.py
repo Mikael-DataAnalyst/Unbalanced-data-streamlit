87c4624bb7aeb2da7273a58accdf171efc02725a
Mikael Andre <mikael.andre12@gmail.com>
2022-11-25 09:12:51 +0100

    Ajout shap_values

--- credit_app.py
+++ credit_app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import os
+import pickle
 import json
 
 
-st.write("test1qsdfsqdf")
-st.write("test1qsdfsqdf")
-
-# Chargement des données
-
+##########################
+# Chargement des données #
+##########################
 # Images
 pouce_vert = Image.open('images/pouce_vert.png')
 pouce_rouge = Image.open("images/pouce_rouge.png")
@@ -18,10 +16,10 @@ logo = Image.open("images/logo.jpg")
 # Données
 
 data_test = pd.read_parquet("data/small_test.parquet")
-with open("lgbm_model1.pkl", 'rb') as file:
+with open("data/lgbm_model1.pkl", 'rb') as file:
     model = pickle.load(file)
 col_info = pd.read_csv("data/col_info.csv")
-with open("shap_values.pkl", 'rb') as file :
+with open("data/shap_values.pkl", 'rb') as file :
     shap_values = pickle.load(file)
 with open("data/dict_nn.txt") as file :
     tmp = file.read()
